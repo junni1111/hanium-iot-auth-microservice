c16951518ca7ad872a2582fe71603655c6d60158
KGESH <baram789456@gmail.com>
2022-06-15 19:16:41 +0900

    Create Ping Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern, Payload, Transport } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   @Get('/')
   healthCheck(): string {
     return `ok`;
+  }
+
+  @MessagePattern('ping', Transport.TCP)
+  async pingToDeviceMicroservice(@Payload() payload: string): Promise<string> {
+    console.log(`Ping from api gateway`, payload);
+    return 'auth-pong';
   }
 }
