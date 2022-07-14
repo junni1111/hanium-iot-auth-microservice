417633c4eff5a6ba23783861a67c233e91c72589
junni1111 <wkszlf98@naver.com>
2022-07-14 20:44:30 +0900

    auth MS 통신 방식 리팩토링 (RPC -> Rest)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,57 +1,35 @@
 import {
   Controller,
-  InternalServerErrorException,
   Logger,
-  NotFoundException,
-  UnauthorizedException,
+  Post,
   UseGuards,
+  Req,
+  Query,
+  Get,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  @MessagePattern({ cmd: 'sign_in' })
-  async login(@Payload() { user }: any) {
-    return this.authService.login(user);
+  @Post('signin')
+  async signIn(@Req() req: any) {
+    return this.authService.signIn(req.user);
   }
 
-  @MessagePattern({ cmd: 'refresh' })
-  refresh(@Payload() tokens: any) {
-    try {
-      return this.authService.regenerateAccessToken(
-        tokens.accessToken,
-        tokens.refreshToken,
-      );
-    } catch (e) {
-      Logger.error(e);
-    }
+  @Get('refresh')
+  refresh(
+    @Query('access') accessToken: string,
+    @Query('refresh') refreshToken: string,
+  ) {
+    return this.authService.regenerateAccessToken(accessToken, refreshToken);
   }
 
-  @MessagePattern({ cmd: 'jwt' })
-  async auth(@Payload() jwt: string) {
-    try {
-      return this.authService.validateToken(jwt);
-    } catch (e) {
-      Logger.debug(e.name);
-      Logger.debug(e.message);
-
-      switch (e.name) {
-        case 'TokenExpiredError':
-          /** Todo: Frontend에서 401 에러받으면 Refresh Token 재발급 요청 */
-          return new UnauthorizedException('Token 유효 시간 끝남');
-        case 'JsonWebTokenError':
-          return new NotFoundException('Token 유효하지 않음');
-        case 'NotBeforeError':
-          return new NotFoundException(e.message);
-        default:
-          Logger.error(`JWT authentication unhandled exception`, e.message);
-          return new InternalServerErrorException(e.message);
-      }
-    }
+  @Get('jwt')
+  async auth(@Query('jwt') jwt: string) {
+    return this.authService.validateToken(jwt);
   }
 }
--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,29 +1,5 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  Logger,
-} from '@nestjs/common';
-import { AuthService } from '../auth.service';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalAuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    Logger.debug(`Request: `, request);
-    const { email, password } = request.user;
-
-    try {
-      const user = await this.authService.validateUser(email, password);
-      request.user = user;
-      console.log(`In Guard: `, request);
-
-      return user !== null; // 🤔
-    } catch (e) {
-      Logger.error(`Auth Guard Exception`, e);
-      throw e;
-    }
-  }
-}
+export class LocalAuthGuard extends AuthGuard('local') {}
